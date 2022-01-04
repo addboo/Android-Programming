61e2a0893ed7b5d39e3dab6fe9ab29f0d642f275
Andreas Bourakis <bourakis@gmail.com>
2022-01-05 00:19:07 +0200

    Update product.table.sql

--- Examples/Products/CRUDAPI/product.table.sql
+++ Examples/Products/CRUDAPI/product.table.sql
@@ -1,1 +1,5 @@
-CREATE TABLE IF NOT EXISTS product ( ProductID int(11) NOT NULL AUTO_INCREMENT, PName text NOT NULL, Price decimal(10,0) NOT NULL, PRIMARY KEY (ProductID) )
+CREATE TABLE IF NOT EXISTS 
+product ( ProductID int(11) NOT NULL AUTO_INCREMENT, 
+          PName text NOT NULL, 
+          Price decimal(10,0) NOT NULL, 
+PRIMARY KEY (ProductID) )
